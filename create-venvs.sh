85d3eecbd4a02d77fadaee51d65d4540b61e2523
Bart Dorsey <bart@bartdorsey.com>
2024-07-15 10:25:26 -0500

    Adding script to create individual venvs for the different python apps

---
+++ create-venvs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Define an array of paths
+paths=(
+  "inventory/api"
+  "sales/api"
+  "sales/poll"
+  "service/api"
+  "service/poll"
+)
+
+WD=$(pwd)
+
+# Loop through each path
+for path in "${paths[@]}"; do
+  echo "Processing directory: $path"
+
+  # Check if the directory exists
+  if [ -d "$path" ]; then
+    # Navigate to the directory
+    cd "$path" || exit
+    
+    # Create a virtual environment in .venv
+    python3 -m venv .venv
+    
+    # Activate the virtual environment
+    source .venv/bin/activate
+    
+    # Check if requirements.txt exists and install packages
+    if [ -f "requirements.txt" ]; then
+      pip install -r requirements.txt
+    else
+      echo "No requirements.txt found in $path"
+    fi
+    
+    # Deactivate the virtual environment
+    deactivate
+
+    cd $WD
+    
+    echo "Finished processing $path"
+  else
+    echo "Directory $path does not exist"
+  fi
+done
+
+echo "All directories processed."
+
