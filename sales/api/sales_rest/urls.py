c759f2dda92ed9a4ea882dd15a8fe71d7da2e4bf
Andrew <andrew.maza.7@gmail.com>
2024-08-14 10:17:47 -0400

    List Salespeople GET POST and DELETE g2g, List Customers GET POST DELETE g2g, List Sales GET g2g

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_salespeople
+from .views import list_salespeople, show_salesperson, list_customers, show_customer, list_sales
 
 urlpatterns = [
     path(
@@ -7,28 +7,28 @@ urlpatterns = [
         list_salespeople,
         name="list_salespeople",
     ),
-  #  path(
-  #      "salespeople/:id/",
-   #     show_salesperson,
-    #    name="show_salesperson",
-    #),
+    path(
+        "salespeople/<int:id>/",
+        show_salesperson,
+        name="show_salesperson",
+    ),
+    path(
+        "customers/",
+        list_customers,
+        name="list_customers",
+    ),
+    path(
+        "customers/<int:id>/",
+        show_customer,
+        name="show_customer",
+    ),
+    path(
+        "sales/",
+        list_sales,
+        name="list_sales",
+    ),
     #path(
-     #   "customers/",
-      #  list_customers,
-       # name="list_customers",
-    #),
-    #path(
-     #   "api_customers/:id/",
-      #  show_customer,
-       # name="show_customer",
-    #),
-    #path(
-     #   "sales/",
-      #  list_sales,
-       # name="list_sales",
-    #),
-    #path(
-     #   "sales/:id/",
+     #   "sales/<:id>/",
       #  show_sale,
        # name="show_sale",
     #)
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -3,7 +3,26 @@ from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 import json
 from common.json import ModelEncoder
-from .models import Salesperson
+from .models import Salesperson, Customer, Sale, AutomobileVO
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+
+    properties = [
+        "vin",
+        "sold",
+    ]
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+    ]
 
 class SalespersonEncoder(ModelEncoder):
     model = Salesperson
@@ -15,11 +34,27 @@ class SalespersonEncoder(ModelEncoder):
         "employee_id",
     ]
 
+class SaleEncoder(ModelEncoder):
+    model = Sale
+
+    properties = [
+        "id",
+        "price",
+        "automobile",
+        "salesperson",
+        "customer",
+    ]
+
+    encoders = {
+        "automobile": AutomobileVOEncoder(),
+        "salesperson": SalespersonEncoder(),
+        "customer": CustomerEncoder(),
+    }
+
 # Create your views here.
-#This function will be for listing the salespeople
+#This function will be for listing the salespeople. This is g2g
 @require_http_methods(["GET", "POST"])
 def list_salespeople(request):
-    print("Get request working")
     if request.method == "GET":
         salespeople = Salesperson.objects.all()
         return JsonResponse(
@@ -28,25 +63,59 @@ def list_salespeople(request):
         )
     else:
         content = json.loads(request.body)
-        salesperson = Salesperson.object.create(**content)
+        salesperson = Salesperson.objects.create(**content)
         return JsonResponse(
             salesperson,
             encoder=SalespersonEncoder,
             safe=False,
         )
 
-#@require_http_methods(["DELETE", "GET"])
-#def show_salesperson(request, id):
- #   if request.method == "GET":
-  #      try:
-   #         salesperson = Salesperson.objects.get(id=id)
-    #        return JsonResponse(
-     #           salesperson,
-      #          encoder=SalespersonEncoder,
-       #         safe=False
-        #    )
-    #try:
-     #   if request.method == "DELETE":
-      #      count, _ = Salesperson.objects.filter(id=id).delete()
-       #     return JsonResponse({ "deleted": count > 0})
-       # e
+#Delete g2g
+@require_http_methods(["DELETE", "GET"])
+def show_salesperson(request, id):
+    if request.method == "DELETE":
+        count, _ = Salesperson.objects.filter(id=id).delete()
+        return JsonResponse({ "deleted": count > 0})
+
+#The code below is to list and create a customer. This is g2g
+@require_http_methods(["GET", "POST"])
+def list_customers(request):
+    if request.method == "GET":
+        customers = Customer.objects.all()
+        return JsonResponse(
+            {"customers": customers},
+            encoder=CustomerEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        customer = Customer.objects.create(**content)
+        return JsonResponse(
+            customer,
+            encoder=CustomerEncoder,
+            safe=False,
+        )
+
+#Delete g2g
+@require_http_methods(["DELETE", "GET"])
+def show_customer(request, id):
+    if request.method == "DELETE":
+        count, _ = Customer.objects.filter(id=id).delete()
+        return JsonResponse({ "deleted": count > 0})
+
+#This is get/create for Sales
+@require_http_methods(["GET", "POST"])
+def list_sales(request):
+    if request.method == "GET":
+        sales = Sale.objects.all()
+        return JsonResponse(
+            {"sales": sales},
+            encoder=SaleEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        sale = Sale.objects.create(**content)
+        return JsonResponse(
+            sale,
+            encoder=SaleEncoder,
+            safe=False,
+        )
