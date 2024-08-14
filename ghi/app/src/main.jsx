5c8c6ac07aa3c2265e82110532541de168c7da2b
jveliquette <juliaveliquette@gmail.com>
2024-08-14 09:41:06 -0700

    moved LoadData function to it's own file, cleaned up main.jsx

--- ghi/app/src/main.jsx
+++ ghi/app/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import loadData from "./LoadData.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,31 +15,4 @@ root.render(
 	</React.StrictMode>
 );
 
-async function loadData() {
-	try {
-		const [manufacturerResponse, modelsResponse, autosResponse] = await Promise.all([
-			fetch('http://localhost:8100/api/manufacturers/'),
-            fetch('http://localhost:8100/api/models/'),
-			fetch('http://localhost:8100/api/automobiles/'),
-		]);
-
-		if (manufacturerResponse.ok && modelsResponse.ok && autosResponse.ok) {
-			const manufacturersData = await manufacturerResponse.json();
-			const modelsData = await modelsResponse.json();
-			const autosData = await autosResponse.json();
-
-			root.render(
-				<React.StrictMode>
-					<BrowserRouter>
-						<App manufacturers={manufacturersData.manufacturers} models={modelsData.models} autos={autosData.autos}/>
-					</BrowserRouter>
-				</React.StrictMode>
-			);
-		} else {
-			console.error(response);
-		}
-	} catch (error) {
-		console.error('Error fetching data:', error);
-	}
-}
-loadData();
+loadData(root);
