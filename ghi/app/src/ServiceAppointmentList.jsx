dd45f62ece0782a0d2ea3681e8cae30c80bf35c0
jveliquette <juliaveliquette@gmail.com>
2024-08-13 17:21:13 -0700

    Started ServiceAppointmentList component - last worked on filtering to only show appointments with the status of "scheduled"

--- ghi/app/src/ServiceAppointmentList.jsx
+++ ghi/app/src/ServiceAppointmentList.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 function ServiceAppointmentList() {
+    const [appointments, setAppointments] = useState([]);
+
+    // Fetch scheduled appointments
+    const fetchAppointments = async () => {
+        const url = "http://localhost:8080/api/appointments/";
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // filter scheduled appointments
+            const scheduledAppointments = data.appointments.filter(appointment => appointment.status === "scheduled");
+            setAppointments(scheduledAppointments)
+        } else {
+            console.error("Failed to load appointments")
+        }
+    };
+
     return (
         <>
             <h1>Service Appointments</h1>
@@ -9,7 +27,10 @@ function ServiceAppointmentList() {
                         <th>Is VIP?</th>
                         <th>Customer</th>
                         <th>Date</th>
-                        <th></th>
+                        <th>Time</th>
+                        <th>Technician</th>
+                        <th>Reason</th>
+                        <th>Update Status</th>
                     </tr>
                 </thead>
             </table>
