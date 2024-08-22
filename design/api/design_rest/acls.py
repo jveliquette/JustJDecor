aadcd1eaff668a5af2a2acd4d9d1902696a32fe5
jveliquette <juliaveliquette@gmail.com>
2024-08-22 11:48:39 -0700

    Added views, added pexels api, added search url

---
+++ design/api/design_rest/acls.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import os
+
+PEXELS_API_KEY = os.environ["PEXELS_API_KEY"]
+
+def search_inspiration(query):
+    headers = {"Authorization": PEXELS_API_KEY}
+    params = {
+        "query": query,
+        "per_page": 10,
+    }
+    url = "https://api.pexels.com/v1/search"
+    response = requests.get(url, params=params, headers=headers)
+    content = json.loads(response.content)
+
+    photos = []
+    try:
+        for photo in content["photos"]:
+            photos.append({
+                "picture_url": photo["src"]["original"],
+            })
+        return {"photos": photos}
+    except KeyError:
+        return {"photos": []}
--- design/api/design_rest/urls.py
+++ design/api/design_rest/urls.py
@@ -1,20 +1,26 @@
 from django.urls import path
-from .views import api_rooms, api_room, api_projects, api_project, api_wishlist_items, api_wishlist_item, api_pins, api_pin, api_user_profiles, api_user_profile
+from .views import (
+    api_rooms, api_room, api_projects, api_project,
+    api_wishlist_items, api_wishlist_item, api_pins,
+    api_pin, api_user_profiles, api_user_profile,
+    api_search_inspiration,
+)
 
 urlpatterns = [
     path('rooms/', api_rooms, name='api_rooms'),
-    path("rooms/<int:pk>/", api_room, name="api_room"),
+    path('rooms/<int:pk>/', api_room, name='api_room'),
 
     path('projects/', api_projects, name='api_projects'),
-    path("projects/<int:id>/", api_project, name="api_project"),
-
+    path('projects/<int:id>/', api_project, name='api_project'),
 
     path('wishlist-items/', api_wishlist_items, name='api_wishlist_items'),
-    path("wishlist/<int:id>/", api_wishlist_item, name="api_wishlist_item"),
+    path('wishlist-items/<int:id>/', api_wishlist_item, name='api_wishlist_item'),
 
     path('pins/', api_pins, name='api_pins'),
-    path('pins/<int:id>/', api_pin, name="api_pin"),
+    path('pins/<int:id>/', api_pin, name='api_pin'),
 
     path('user-profiles/', api_user_profiles, name='api_user_profiles'),
-    path("user-profiles/<int:id>/", api_user_profile, name="api_user_profile"),
+    path('user-profiles/<int:id>/', api_user_profile, name='api_user_profile'),
+
+    path('search-inspiration/', api_search_inspiration, name='api_search_inspiration'),
 ]
